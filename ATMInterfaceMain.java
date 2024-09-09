b518120e91f78fe690eb731553999374c2740216
CharumathyB <163385233+CharumathyB@users.noreply.github.com>
2024-09-09 15:30:58 +0530

    Delete ATMInterfaceMain.java

--- ATMInterfaceMain.java
+++
@@ -1,8 +0,0 @@
-package AtmInterface;
-public class ATMInterfaceMain {
-    public static void main(String[] args) {
-        BankAccountInterface userAccount = new BankAccount(100000);
-        ATMInterface atm = new ATM(userAccount);
-        atm.start();
-    }
-}
